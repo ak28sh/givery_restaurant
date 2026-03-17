333fcf4fc10ea55f9c1e52d9c732ffba318ce9d4
ak28sh <akshatakotti28@gmail.com>
2026-03-18 02:56:35 +0900

    add external database

--- db/database.py
+++ db/database.py
@@ -1,9 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:password@localhost/restaurant"
+# DATABASE_URL = "mysql+pymysql://root:password@localhost/restaurant"
+# engine = create_engine(DATABASE_URL)
 
+# DATABASE_URL = "sqlite:///./restaurant.db"
+# engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+DATABASE_URL = "mysql+pymysql://root:BBKUArAGwCWGgoASdzLQZfHxPxsCUyWx@caboose.proxy.rlwy.net:24476/railway"
 engine = create_engine(DATABASE_URL)
+
+
 print("Database connection successful")
 SessionLocal = sessionmaker(bind=engine)
 
