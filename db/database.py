2d7be97ad5db942b7a79e1b939aef39493e8c371
ak28sh <akshatakotti28@gmail.com>
2026-03-18 01:50:05 +0900

    add database file

--- db/database.py
+++ db/database.py
@@ -3,13 +3,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "mysql+pymysql://root:password@localhost/restaurant"
 
-try:
-    engine = create_engine(DATABASE_URL)
-    engine.connect()
-    print("Database connection successful")
-    SessionLocal = sessionmaker(bind=engine)
-except Exception as e:
-    print(f"Database connection failed: {e}")
+engine = create_engine(DATABASE_URL)
+print("Database connection successful")
+SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
