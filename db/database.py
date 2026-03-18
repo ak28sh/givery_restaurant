bc173b9f473604da77b818dffe2d057a198768d1
ak28sh <akshatakotti28@gmail.com>
2026-03-18 11:57:44 +0900

    enhance

--- db/database.py
+++ db/database.py
@@ -1,22 +1,33 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# DATABASE_URL = "mysql+pymysql://root:password@localhost/restaurant"
-# engine = create_engine(DATABASE_URL)
+# Load environment variables
+load_dotenv()
 
-# DATABASE_URL = "sqlite:///./restaurant.db"
-# engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# Get DB URL from .env
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = "mysql+pymysql://root:BBKUArAGwCWGgoASdzLQZfHxPxsCUyWx@caboose.proxy.rlwy.net:24476/railway"
+print(DATABASE_URL)
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set in .env")
+
+# Create engine
 engine = create_engine(DATABASE_URL)
 
-
 print("Database connection successful")
-SessionLocal = sessionmaker(bind=engine)
 
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
+
 def get_db():
+    """
+    Dependency to get DB session.
+    Ensures session is closed after request.
+    """
     db = SessionLocal()
     try:
         yield db
